d9e2997e778458707aa9591b2f014d5fb81e2725
lantian-zhao <64811288+lantian-zhao@users.noreply.github.com>
2021-10-28 19:23:14 -0500

    Add files via upload

---
+++ app/src/main/java/com/example/team404/Content.java
@@ -0,0 +1,45 @@
+package com.example.team404;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+public class Content extends ArrayAdapter<Habit> {
+private ArrayList<Habit> habits;
+private Context context;
+
+public Content(Context context, ArrayList<Habit> habits){
+    super(context,0,habits);
+    this.habits = habits;
+    this.context=context;
+
+}
+public View GetView(int position, View convertView, ViewGroup parent){
+    View view = convertView;
+    if(view == null){
+        view = LayoutInflater.from(context).inflate((R.layout.habit_content),parent,false);
+
+    }
+    Habit habit = habits.get(position);
+
+    TextView habitTitle = view.findViewById(R.id.titleTextView);
+    TextView habitReason = view.findViewById(R.id.reasonTextView);
+    TextView habitDate = view.findViewById(R.id.dateTextView);
+
+    habitTitle.setText(habit.getTitle());
+    SimpleDateFormat formatter = new SimpleDateFormat(("dd.MM.yyyy"));
+    String date = formatter.format(habit.getStartDate());
+    habitDate.setText(date);
+    habitReason.setText(habit.getReason());
+    return view;
+
+}
+}
