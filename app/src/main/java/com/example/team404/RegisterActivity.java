d6ec05cf9ba7e62319b9ab0585f7e25a20f56994
iris <77365934+irriss-nn@users.noreply.github.com>
2021-11-02 17:07:37 -0600

    11

---
+++ app/src/main/java/com/example/team404/RegisterActivity.java
@@ -0,0 +1,84 @@
+package com.example.team404;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener{
+    private TextView register;
+    private EditText password;
+    private EditText repassword;
+    private EditText phone;
+    private EditText email;
+    private EditText name;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        register = findViewById(R.id.reg_btn);
+        register.setOnClickListener(this);
+        email = findViewById(R.id.reg_email);
+        password = findViewById(R.id.password);
+        repassword = findViewById(R.id.repassward);
+        phone = findViewById(R.id.reg_phone);
+        name = findViewById(R.id.reg_phone);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
+
+
+    }
+    public Boolean isEmail(String str) {
+        boolean isEmail = false;
+        String expr = "^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";
+        if (str.matches(expr)) {
+            isEmail = true;
+        }
+        return isEmail;
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        String emailAddress = email.getText().toString().trim();
+        String userPassword = password.getText().toString().trim();
+        String userName = name.getText().toString().trim();
+        String userRepassword = repassword.getText().toString().trim();
+
+
+        if (view.getId() == R.id.reg_btn) {
+            if (TextUtils.isEmpty(emailAddress) || !isEmail(emailAddress)) {
+                Toast.makeText(RegisterActivity.this, " email account error!",
+                        Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (TextUtils.isEmpty(userName)) {
+                name.setError("Name is required!");
+                name.requestFocus();
+                return;
+            }
+            if (TextUtils.isEmpty(phone.getText().toString())) {
+                phone.setError("Phone number is required!");
+                phone.requestFocus();
+                return;
+
+            }
+        }
+
+        switch (view.getId()){
+            case R.id.reg_btn:
+                startActivity(new Intent(this,LoginActivity.class));
+
+        }
+    }
+}
