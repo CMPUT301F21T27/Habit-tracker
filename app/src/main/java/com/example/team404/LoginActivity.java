96ddba5576abe11a8412a6d93eb10c39945f4c4f
WentaiDu <78457517+WentaiDu@users.noreply.github.com>
2021-11-04 00:31:54 -0600

    login

--- app/src/main/java/com/example/team404/LoginActivity.java
+++ app/src/main/java/com/example/team404/LoginActivity.java
@@ -1,4 +1,100 @@
 package com.example.team404;
 
-public class LoginActivity {
+import static android.content.ContentValues.TAG;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button signup_btn;
+    private Button sign_in_btn;
+    private EditText user_email;
+    private EditText user_pass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        signup_btn = findViewById(R.id.signup_btn);
+        sign_in_btn = findViewById(R.id.sign_in_btn);
+        user_email = findViewById(R.id.user_email);
+        user_pass = findViewById(R.id.user_pass);
+        signup_btn.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.register);
+            }
+        });
+        final FirebaseFirestore db;
+        db = FirebaseFirestore.getInstance();
+        sign_in_btn.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                final String emailAddress = user_email.getText().toString();
+                final String userPassword = user_pass.getText().toString();
+                //check length for email
+                DocumentReference docRef = db.collection("User").document(emailAddress);
+                        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    DocumentSnapshot document = task.getResult();
+                                    if (document.exists()) {
+                                        String real_passward=document.getString("userPassword").toString();
+                                        if (real_passward.equals(userPassword)){
+                                            Toast.makeText(LoginActivity.this, "they are match!you are login",
+                                            Toast.LENGTH_SHORT).show();
+                                            setContentView(R.layout.activity_main);
+                                            return;
+                                        }
+                                        else{
+                                            Toast.makeText(LoginActivity.this, "they are not match!",
+                                            Toast.LENGTH_SHORT).show();}
+                                    }
+                                    else {
+                                        Toast.makeText(LoginActivity.this, "No this email account!",
+                                        Toast.LENGTH_SHORT).show();
+                                    }
+                                } else {
+                                    Log.d(TAG, "get failed with ", task.getException());
+                                }
+                            }
+                        });
+
+
+
+            }
+        });
+    }
 }
---
+++ app/src/main/java/com/example/team404/RegisterActivity.java
@@ -0,0 +1,113 @@
+package com.example.team404;
+
+import static android.content.ContentValues.TAG;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterActivity extends AppCompatActivity{
+    private Button reg_btn;
+    private EditText password;
+    private EditText repassword;
+    private EditText phone;
+    private EditText email;
+    private EditText name;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+
+        reg_btn = findViewById(R.id.reg_btn);
+        email = findViewById(R.id.reg_email);
+        password = findViewById(R.id.password);
+        repassword = findViewById(R.id.repassward);
+        phone = findViewById(R.id.reg_phone);
+        name = findViewById(R.id.reg_phone);
+        final FirebaseFirestore db;
+        db = FirebaseFirestore.getInstance();
+        final CollectionReference collectionReference = db.collection("User");
+
+        reg_btn.setOnClickListener( new View.OnClickListener(){
+        @Override
+            public void onClick(View view) {
+                final String emailAddress = email.getText().toString();
+                final String userPassword = password.getText().toString();
+                final String userName = name.getText().toString();
+                final String userRepassword = repassword.getText().toString();
+                final String userphone = phone.getText().toString();
+                // Get a top level reference to the collection
+                Map<String, Object> city = new HashMap<>();
+                city.put("userPassword", userPassword);
+                city.put("emailAddress", emailAddress);
+                city.put("userName", userName);
+                city.put("Habits", "");
+                city.put("Habitsevent", "");
+                city.put("phone",userphone);
+                collectionReference
+                        .document(emailAddress)
+                        .set(city)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Log.d(TAG, "DocumentSnapshot successfully written!");
+                                setContentView(R.layout.login);
+                            }
+                        })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w(TAG, "Error writing document", e);
+                        }
+                    });
+
+
+
+            }
+        });
+    }
+}
+
