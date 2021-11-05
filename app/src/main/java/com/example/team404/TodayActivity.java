e290e35d0c5e2f36e8e6ff1c13c8c8cfd56a99a9
jackie174 <pcheng1@ualberta.ca>
2021-11-05 03:48:45 -0600

    fix back

--- app/src/main/java/com/example/team404/TodayActivity.java
+++ app/src/main/java/com/example/team404/TodayActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -14,16 +15,17 @@ import java.util.ArrayList;
 
 public class TodayActivity extends AppCompatActivity {
     ListView today_habit;
-    Button back;
+    ImageView back;
     ArrayAdapter<Habit> habitArrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_today);
 
         today_habit=(ListView) findViewById(R.id.today_habit_list);
-        back=(Button)findViewById(R.id.back_button);
+        back= findViewById(R.id.backImage);
         Intent intent = getIntent();
         ArrayList<Habit> habitsToday = new ArrayList<Habit>();
         habitsToday = (ArrayList<Habit>)getIntent().getSerializableExtra("habitsToday");
