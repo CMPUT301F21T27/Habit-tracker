dd019faa61f2713e79ac16c6a6695b64e6dd543c
jackie174 <pcheng1@ualberta.ca>
2021-11-11 22:33:43 -0700

    updated login

--- app/src/main/java/com/example/team404/LoginActivity.java
+++ app/src/main/java/com/example/team404/LoginActivity.java
@@ -20,6 +20,9 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -40,11 +43,15 @@ public class LoginActivity extends AppCompatActivity {
     private Button sign_in_btn;
     private EditText user_email;
     private EditText user_pass;
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
+    static protected DocumentReference currentUserRef = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        mAuth = FirebaseAuth.getInstance();
 
         signup_btn = findViewById(R.id.signup_btn);
         sign_in_btn = findViewById(R.id.sign_in_btn);
@@ -60,13 +67,49 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        final FirebaseFirestore db;
+
+
         db = FirebaseFirestore.getInstance();
         sign_in_btn.setOnClickListener( new View.OnClickListener(){
             @Override
             public void onClick(View view) {
                 final String emailAddress = user_email.getText().toString();
                 final String userPassword = user_pass.getText().toString();
+
+                if (emailAddress.equals("")||userPassword.equals("")){
+                    Toast.makeText(LoginActivity.this, "Please input email and password!", Toast.LENGTH_SHORT).show();
+                }
+
+
+
+                mAuth.signInWithEmailAndPassword(emailAddress, userPassword)
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    FirebaseUser real_user = mAuth.getCurrentUser();
+                                    currentUserRef = db.collection("users").document(real_user.getEmail());
+                                    String userEmail = mAuth.getCurrentUser().getEmail();
+
+                                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                    startActivity(intent);
+                                } else {
+                                    Toast.makeText(LoginActivity.this, "password/email is incorrect!", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(LoginActivity.this, "Log in failed!",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+
+
+                /*
+
                 //check length for email
                 DocumentReference docRef = db.collection("User").document(emailAddress);
                         docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
@@ -98,9 +141,20 @@ public class LoginActivity extends AppCompatActivity {
                             }
                         });
 
+                 */
+
 
 
             }
         });
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            currentUser.reload();
+        }
     }
 }
--- app/src/main/java/com/example/team404/RegisterActivity.java
+++ app/src/main/java/com/example/team404/RegisterActivity.java
@@ -4,6 +4,7 @@ import static android.content.ContentValues.TAG;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.UserManager;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
@@ -22,10 +23,13 @@ import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
-
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -47,6 +51,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 public class RegisterActivity extends AppCompatActivity{
     private Button reg_btn;
@@ -55,30 +60,121 @@ public class RegisterActivity extends AppCompatActivity{
     private EditText phone;
     private EditText email;
     private EditText name;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
+        mAuth = FirebaseAuth.getInstance();
+
 
         reg_btn = findViewById(R.id.reg_btn);
         email = findViewById(R.id.reg_email);
         password = findViewById(R.id.password);
         repassword = findViewById(R.id.repassward);
         phone = findViewById(R.id.reg_phone);
-        name = findViewById(R.id.reg_phone);
-        final FirebaseFirestore db;
-        db = FirebaseFirestore.getInstance();
-        final CollectionReference collectionReference = db.collection("User");
+        name = findViewById(R.id.reg_name);
+        //final FirebaseFirestore db;
+        //db = FirebaseFirestore.getInstance();
+        //final CollectionReference collectionReference = db.collection("User");
 
         reg_btn.setOnClickListener( new View.OnClickListener(){
         @Override
             public void onClick(View view) {
-                final String emailAddress = email.getText().toString();
-                final String userPassword = password.getText().toString();
-                final String userName = name.getText().toString();
-                final String userRepassword = repassword.getText().toString();
-                final String userphone = phone.getText().toString();
+                String emailAddress = email.getText().toString();
+                String userPassword = password.getText().toString();
+                String userName = name.getText().toString();
+                String userRepassword = repassword.getText().toString();
+                String userphone = phone.getText().toString();
+                //https://stackoverflow.com/questions/8204680/java-regex-email
+                //Nov 20, 2011
+                //author:Jason Buberel;
+                //check email form
+                if ( emailAddress != null && Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE).matcher(emailAddress).find()){
+                    //emailAddress.matches("[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}")
+                }else{
+                    Toast.makeText(RegisterActivity.this, "Invalid email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (userName.length()==0){
+                    Toast.makeText(RegisterActivity.this, "user name is empty", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (userphone.length()==0){
+                    Toast.makeText(RegisterActivity.this, "user phone is empty", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (userPassword.length()==0){
+                    Toast.makeText(RegisterActivity.this, "password is empty", Toast.LENGTH_SHORT).show();
+                    return;
+                }else if (userPassword.length()< 6){
+                    Toast.makeText(RegisterActivity.this, "password must contain at least 6 character", Toast.LENGTH_SHORT).show();
+                    return;
+                }else if (userPassword.length()>10){
+                    Toast.makeText(RegisterActivity.this, "password must contain at moat 10 character", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (userRepassword==null){
+                    Toast.makeText(RegisterActivity.this, "Re-password is empty", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (!userPassword.equals(userRepassword)){
+                    Toast.makeText(RegisterActivity.this, "password does not match", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+
+                mAuth.createUserWithEmailAndPassword(emailAddress, userPassword)
+                        .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()){
+                                    FirebaseUser firebaseuser = mAuth.getCurrentUser();
+                                    Map<String, Object> city = new HashMap<>();
+                                    city.put("userPassword", userPassword);
+                                    city.put("emailAddress", emailAddress);
+                                    city.put("userName", userName);
+                                    city.put("Habits", "");
+                                    city.put("Habitsevent", "");
+                                    city.put("phone",userphone);
+
+                                    FirebaseFirestore db= FirebaseFirestore.getInstance();
+                                    final CollectionReference collectionReference = db.collection("User");
+
+                                    collectionReference
+                                            .document(emailAddress)
+                                            .set(city)
+                                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                @Override
+                                                public void onSuccess(Void aVoid) {
+                                                    Log.d(TAG, "DocumentSnapshot successfully written!");
+
+                                                    //setContentView(R.layout.login);
+                                                    Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                                                    startActivity(intent);
+                                                }
+                                            })
+                                            .addOnFailureListener(new OnFailureListener() {
+                                                @Override
+                                                public void onFailure(@NonNull Exception e) {
+                                                    Log.w(TAG, "Error writing document", e);
+                                                    Toast.makeText(RegisterActivity.this, "Sign up failed!!", Toast.LENGTH_SHORT).show();
+                                                }
+                                            });
+
+                                }else{
+                                    Toast.makeText(RegisterActivity.this, userPassword, Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(RegisterActivity.this, "Sign up failed!", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }
+
+                            );
+                /*
                 // Get a top level reference to the collection
                 Map<String, Object> city = new HashMap<>();
                 city.put("userPassword", userPassword);
@@ -105,6 +201,8 @@ public class RegisterActivity extends AppCompatActivity{
                             Log.w(TAG, "Error writing document", e);
                         }
                     });
+
+                 */
 
 
 
