78d081ada6e01d071f44b5cfcac9ae5afc2acb2a
jackie174 <pcheng1@ualberta.ca>
2021-11-29 08:13:36 -0700

    Update MainActivityIntentTest.java

--- app/src/androidTest/java/com/example/team404/MainActivityIntentTest.java
+++ app/src/androidTest/java/com/example/team404/MainActivityIntentTest.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 
+import com.example.team404.Account.AccountActivity;
 import com.example.team404.Habit.Habit;
 import com.example.team404.HabitEvent.HabitEventListActivity;
 import com.example.team404.Login.LoginActivity;
@@ -21,6 +22,12 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 public class MainActivityIntentTest {
+    // I use Pixel 4 API 29,
+    // so I set each navigation bar for each coordinate is :
+    //Home: 0, 2000
+    //Subscribe: 500, 2000
+    //My habit: 800, 2000
+    //Account: 1000, 2000
     private Solo solo;
     @Rule
     public ActivityTestRule<LoginActivity> rule = new ActivityTestRule<>(LoginActivity.class, true,true);
@@ -37,7 +44,7 @@ public class MainActivityIntentTest {
     }
     // test home page
     // if there is some habit from firebase in the home page
-    
+
     @Test
     public void MainListTest() throws Exception{
         //solo.assertCurrentActivity("current Activity", LoginActivity.class);
@@ -65,7 +72,106 @@ public class MainActivityIntentTest {
         }
 
 
+
+
     }
+    @Test
+    public void GoToMyHabitActivityTest() throws Exception{
+        //solo.assertCurrentActivity("current Activity", LoginActivity.class);
+        solo.getCurrentActivity();
+        System.out.println("---"+solo.getCurrentActivity());
+        EditText password = (EditText) solo.getView(R.id.user_pass);
+        EditText email = (EditText) solo.getView(R.id.user_email);
+        solo.enterText(email, "test@test.com");
+        solo.enterText(password, "123123");
+        solo.clickOnButton("Sign In");
+
+        solo.assertCurrentActivity("current Activity",MainActivity.class);
+        solo.waitForActivity(MainActivity.class, 3000);
+        //I set the time is 6000, it really depend on the internet
+        //it needs some times to reload the list of habit from Firebase
+        // if it is not pass, you just need to extends the time, until the list of habit is show in the list
+        ListView habit_list_in_home_page = solo.getCurrentActivity().findViewById(R.id.main_list);
+        Thread.sleep(6000);
+        Habit habit = (Habit) habit_list_in_home_page.getItemAtPosition(0);
+
+        if (habit!=null){
+
+        }else{
+            throw  new AssertionError();
+        }
+
+        solo.clickOnScreen(800, 2000);
+
+        solo.assertCurrentActivity("Current Activity", MyActivity.class);
+
+
+    }
+    @Test
+    public void GoToSubscribeActivityTest() throws Exception{
+        //solo.assertCurrentActivity("current Activity", LoginActivity.class);
+        solo.getCurrentActivity();
+        System.out.println("---"+solo.getCurrentActivity());
+        EditText password = (EditText) solo.getView(R.id.user_pass);
+        EditText email = (EditText) solo.getView(R.id.user_email);
+        solo.enterText(email, "test@test.com");
+        solo.enterText(password, "123123");
+        solo.clickOnButton("Sign In");
+
+        solo.assertCurrentActivity("current Activity",MainActivity.class);
+        solo.waitForActivity(MainActivity.class, 3000);
+        //I set the time is 6000, it really depend on the internet
+        //it needs some times to reload the list of habit from Firebase
+        // if it is not pass, you just need to extends the time, until the list of habit is show in the list
+        ListView habit_list_in_home_page = solo.getCurrentActivity().findViewById(R.id.main_list);
+        Thread.sleep(6000);
+        Habit habit = (Habit) habit_list_in_home_page.getItemAtPosition(0);
+
+        if (habit!=null){
+
+        }else{
+            throw  new AssertionError();
+        }
+
+        solo.clickOnScreen(500, 2000);
+
+        solo.assertCurrentActivity("Current Activity", SubscribeActivity.class);
+
+
+    }
+    @Test
+    public void GoToAccountActivityTest() throws Exception{
+        //solo.assertCurrentActivity("current Activity", LoginActivity.class);
+        solo.getCurrentActivity();
+        System.out.println("---"+solo.getCurrentActivity());
+        EditText password = (EditText) solo.getView(R.id.user_pass);
+        EditText email = (EditText) solo.getView(R.id.user_email);
+        solo.enterText(email, "test@test.com");
+        solo.enterText(password, "123123");
+        solo.clickOnButton("Sign In");
+
+        solo.assertCurrentActivity("current Activity",MainActivity.class);
+        solo.waitForActivity(MainActivity.class, 3000);
+        //I set the time is 6000, it really depend on the internet
+        //it needs some times to reload the list of habit from Firebase
+        // if it is not pass, you just need to extends the time, until the list of habit is show in the list
+        ListView habit_list_in_home_page = solo.getCurrentActivity().findViewById(R.id.main_list);
+        Thread.sleep(6000);
+        Habit habit = (Habit) habit_list_in_home_page.getItemAtPosition(0);
+
+        if (habit!=null){
+
+        }else{
+            throw  new AssertionError();
+        }
+
+        solo.clickOnScreen(1000, 2000);
+
+        solo.assertCurrentActivity("Current Activity", AccountActivity.class);
+
+
+    }
+
 
 
 }
